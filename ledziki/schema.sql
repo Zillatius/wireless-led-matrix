5f0472f14fd37e61888042951732e3215acc89f6
Zillatius <mlmaciek4@gmail.com>
2021-06-02 10:31:11 +0100

    deleting old files

--- ledziki/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS measurements;
-
-CREATE TABLE measurements(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    temperature REAL,
-    humidity REAL,
-    measurement_date INTEGER(4) DEFAULT (strftime('%s', 'now'))
-);
